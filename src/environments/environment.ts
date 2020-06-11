885c2aa59af97918a2b3015afc5d0a9d7862b3a6
Pratik <pratik1070061@gmail.com>
2020-06-10 21:34:44 -0400

    Fixed Navigation Highlights

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  gatewayUrl: 'http://localhost:8080'
+  gatewayUrl: 'http://neion-gw-development.apps.openshift.ne-innovation.com'
   //gatewayUrl : 'http://lrqi-gateway-development.apps.openshift.ne-innovation.com/api/gateway'
 };
 
