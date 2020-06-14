3496a5c29d9498cadf72cd9c115d9c274869a028
Pratik <pratik1070061@gmail.com>
2020-06-14 11:48:09 -0400

    updating prod env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gatewayUrl : 'http://lrqi-gateway-development.apps.openshift.ne-innovation.com/api/gateway'
+  gatewayUrl : 'http://neion-gw-development.apps.openshift.ne-innovation.com'
 };
