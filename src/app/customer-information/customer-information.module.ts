4c7f7c601de15fb433f28b25f15d8e517b096e55
Pratik <pratik1070061@gmail.com>
2020-06-20 10:52:01 -0400

    moving to new namespace

--- src/app/customer-information/customer-information.module.ts
+++ src/app/customer-information/customer-information.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CustomerInfoComponent } from './customer-info/customer-info.component';
-import { CustomerRoutingModule } from './customer-routing-module';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatStepperModule } from '@angular/material/stepper';
-import {FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CustomerInfoComponent } from "./customer-info/customer-info.component";
+import { CustomerRoutingModule } from "./customer-routing-module";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatStepperModule } from "@angular/material/stepper";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatMomentDateModule } from "@angular/material-moment-adapter";
 
 /* NgRx */
-import { StoreModule } from '@ngrx/store';
-import { reducer } from './state/customer.reducer';
-import { EffectsModule } from '@ngrx/effects';
-import { CustomerEffects } from './state/customer.effects';
+import { StoreModule } from "@ngrx/store";
+import { reducer } from "./state/customer.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { CustomerEffects } from "./state/customer.effects";
 
 @NgModule({
   imports: [
@@ -28,14 +28,10 @@ import { CustomerEffects } from './state/customer.effects';
     HttpClientModule,
     MatDatepickerModule,
     MatMomentDateModule,
-    StoreModule.forFeature('customers', reducer),
-    EffectsModule.forFeature([CustomerEffects])
+    StoreModule.forFeature("customers", reducer),
+    EffectsModule.forFeature([CustomerEffects]),
   ],
-  declarations: [
-    CustomerInfoComponent
-  ],
-  exports: [
-    CustomerInfoComponent
-  ]
+  declarations: [CustomerInfoComponent],
+  exports: [CustomerInfoComponent],
 })
-export class CustomerInformationModule { }
+export class CustomerInformationModule {}
