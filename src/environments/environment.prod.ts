61afd4e4e98d7490fc9d7790a6b1ca79d326306c
Pratik <pratik1070061@gmail.com>
2020-07-14 23:11:38 -0400

    changing istio URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gatewayUrl : 'http://istio-ingressgateway-istiosystem.apps.openshift.ne-innovation.com'
+  gatewayUrl : 'http://istio-ingressgateway-istio-system.apps.awsopenshift.ne-innovation.com'
 };
